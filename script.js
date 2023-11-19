51b20168a62e4646a9609726828fe7a98d8b61a5
Abhay Singh <141138878+abhaysantacruz444@users.noreply.github.com>
2023-11-19 13:49:39 +0530

    BMI-Calculator

---
+++ script.js
@@ -0,0 +1,44 @@
+const bmiText = document.getElementById("bmi");
+const descText = document.getElementById("desc");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("reset", onFormReset);
+
+function onFormReset() {
+  bmiText.textContent = 0;
+  bmiText.className = "";
+  descText.textContent = "N/A";
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const weight = parseFloat(form.weight.value);
+  const height = parseFloat(form.height.value);
+
+  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+    alert("Please enter a valid weight and height");
+    return;
+  }
+
+  const heightInMeters = height / 100; 
+  const bmi = weight / Math.pow(heightInMeters, 2);
+  const desc = interpretBMI(bmi);
+
+  bmiText.textContent = bmi.toFixed(2);
+  bmiText.className = desc;
+  descText.innerHTML = `You are <strong>${desc}</strong>`;
+}
+
+function interpretBMI(bmi) {
+  if (bmi < 18.5) {
+    return "underweight";
+  } else if (bmi < 25) {
+    return "healthy";
+  } else if (bmi < 30) {
+    return "overweight";
+  } else {
+    return "obese";
+  }
+}
